a90d06643cedc2bcf6a2424c26c9ceb4cfb4bff9
snowcrab382 <108510951+snowcrab382@users.noreply.github.com>
2023-02-02 23:35:59 +0900

    [Bronze II] Title: 다이얼, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,20 @@
+alphabet = input()
+count = 0
+for i in alphabet:
+    if  i in 'ABC':
+        count += 3
+    elif i in 'DEF':
+        count += 4
+    elif i in 'GHI':
+        count += 5
+    elif i in 'JKL':
+        count += 6
+    elif i in 'MNO':
+        count += 7
+    elif i in 'PQRS':
+        count += 8
+    elif i in 'TUV':
+        count += 9
+    elif i in 'WXYZ':
+        count += 10
+print(count)
