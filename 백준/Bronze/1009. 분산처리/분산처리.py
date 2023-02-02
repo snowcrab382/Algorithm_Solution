b8212b45b62e784ec4e6007ff7cd70c054db323c
snowcrab382 <108510951+snowcrab382@users.noreply.github.com>
2023-02-02 20:19:48 +0900

    [Bronze II] Title: 분산처리, Time: 104 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -0,0 +1,20 @@
+T = int(input())
+for i in range(T):
+    a, b = map(int,input().split())
+    aa = a % 10
+    if aa == 0:
+        print('10')
+    elif aa in [1,5,6]:
+        print(aa)
+    elif aa in [4,9]:
+        bb = b % 2
+        if bb == 0:
+            print((aa**2) % 10)
+        else:
+            print(aa)
+    else:
+        bb = b % 4
+        if bb == 0:
+            print(aa**4%10)
+        else:
+            print(aa**bb%10)
