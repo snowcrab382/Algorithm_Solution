94d84845f821ca1c4dcc0a4529b47dddc791915e
snowcrab382 <108510951+snowcrab382@users.noreply.github.com>
2023-02-07 16:31:22 +0900

    [Silver III] Title: 소수 구하기, Time: 744 ms, Memory: 39068 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,10 +1,21 @@
-M,N = map(int,input().split())
+m,n = map(int,input().split())
+d = [True] * 1000001
+d[0],d[1] = False,False
 
-for i in range(M,N+1):
-    if i == 1:
-        continue
-    for j in range(2,int(i**0.5)+1):
+for i in range(3,1001):
+    for j in range(2,i):
         if i % j == 0:
-            break
-    else:
+            d[i] = False
+            continue
+
+for i in range(2,1001):
+    if d[i] == False:
+        continue
+    x = 2
+    while x*i < len(d):
+        d[x*i] = False
+        x += 1
+
+for i in range(m,n+1):
+    if d[i] == True:
         print(i)
