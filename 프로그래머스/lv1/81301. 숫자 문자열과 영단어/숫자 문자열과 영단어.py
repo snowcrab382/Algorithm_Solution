58e2dca3b64c34cf395b81b4dbfb7ea040484351
snowcrab382 <108510951+snowcrab382@users.noreply.github.com>
2023-02-12 14:49:27 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,14 +1,6 @@
 def solution(s):
-    result = []
-    num = dict(zero=0, one=1, two=2, three=3, four=4, five=5, six=6, seven=7, eight=8, nine=9)
-    temp = ''
-    for i in range(len(s)):
-        if s[i].isdigit():
-            result.append(s[i])
-        else:
-            temp += s[i]
-            if temp in num:
-                result.append(str(num[temp]))
-                temp = ''
-    answer = int(''.join(result))
-    return answer
+    answer = s
+    num = ['zero','one','two','three','four','five','six','seven','eight','nine']
+    for i in range(len(num)):
+        answer = answer.replace(num[i],str(i))
+    return int(answer)
