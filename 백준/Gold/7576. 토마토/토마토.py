0c0084ac044f3850379b6e059dbc6356e064d8d8
snowcrab382 <108510951+snowcrab382@users.noreply.github.com>
2023-02-12 19:42:20 +0900

    [Gold V] Title: 토마토, Time: 1344 ms, Memory: 98688 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,19 +1,16 @@
 from collections import deque
 
-M,N = map(int,input().split())
+n,m = map(int,input().split())
 graph = []
-
-for _ in range(N):
-    graph.append(list(map(int,input().split())))
+queue = deque()
 
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
 
-queue = deque()
-result = 0
-
-for i in range(N):
-    for j in range(M):
+for i in range(m):
+    line = list(map(int,input().split()))
+    graph.append(line)
+    for j in range(n):
         if graph[i][j] == 1:
             queue.append((i,j))
 
@@ -23,21 +20,17 @@ def bfs():
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if nx < 0 or nx >= N or ny < 0 or ny >= M:
-                continue
-            if graph[nx][ny] == -1:
-                continue
-            if graph[nx][ny] == 1:
-                continue
-            if graph[nx][ny] == 0:
+            if 0 <= nx < m and 0 <= ny < n and graph[nx][ny] == 0:
                 graph[nx][ny] = graph[x][y] + 1
                 queue.append((nx,ny))
 
 bfs()
+result = 0
+
 for i in graph:
     for j in i:
-        if j == 0:
+        if j== 0:
             print(-1)
             exit(0)
-    result = max(result, max(i))
-print(result -1)
+    result = max(result,max(i))
+print(result-1)
