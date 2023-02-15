411f6ef3498a7fd4776c8a02dbe5f3d9e4d07514
snowcrab382 <108510951+snowcrab382@users.noreply.github.com>
2023-02-15 19:36:42 +0900

    [Gold V] Title: 적록색약, Time: 92 ms, Memory: 34308 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -3,47 +3,21 @@ from collections import deque
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
 
-def bfs_R(visit,x,y):
-    queue_R = deque()
-    queue_R.append((x,y))
+def bfs(visit,x,y):
+    queue = deque()
+    queue.append((x,y))
     visit[x][y] = 1
-    while queue_R:
-        x,y = queue_R.popleft()
+    current_color = graph[x][y]
+    while queue:
+        x,y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 'R':
+            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == current_color:
                 if not visit[nx][ny]:
                     visit[nx][ny] = 1
-                    queue_R.append((nx,ny))
+                    queue.append((nx,ny))
 
-def bfs_G(visit,x,y):
-    queue_G = deque()
-    queue_G.append((x,y))
-    visit[x][y] = 1
-    while queue_G:
-        x,y = queue_G.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 'G':
-                if not visit[nx][ny]:
-                    visit[nx][ny] = 1
-                    queue_G.append((nx,ny))
-
-def bfs_B(visit,x,y):
-    queue_B = deque()
-    queue_B.append((x,y))
-    visit[x][y] = 1
-    while queue_B:
-        x,y = queue_B.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 'B':
-                if not visit[nx][ny]:
-                    visit[nx][ny] = 1
-                    queue_B.append((nx,ny))
 n = int(input())
 graph = [list(input()) for _ in range(n)]
 visit,visit_RG = [[0] * n for _ in range(n)],[[0] * n for _ in range(n)]
@@ -51,14 +25,8 @@ cnt,cnt_RG = 0,0
 
 for i in range(n):
     for j in range(n):
-        if graph[i][j] == 'R' and not visit[i][j]:
-            bfs_R(visit,i,j)
-            cnt += 1
-        if graph[i][j] == 'G' and not visit[i][j]:
-            bfs_G(visit,i,j)
-            cnt += 1
-        if graph[i][j] == 'B' and not visit[i][j]:
-            bfs_B(visit,i,j)
+        if not visit[i][j]:
+            bfs(visit,i,j)
             cnt += 1
 
 for i in range(n):
@@ -68,11 +36,8 @@ for i in range(n):
 
 for i in range(n):
     for j in range(n):
-        if graph[i][j] == 'R' and not visit_RG[i][j]:
-            bfs_R(visit_RG,i,j)
-            cnt_RG += 1
-        if graph[i][j] == 'B' and not visit_RG[i][j]:
-            bfs_B(visit_RG,i,j)
+        if not visit_RG[i][j]:
+            bfs(visit_RG,i,j)
             cnt_RG += 1
 
 print(cnt,cnt_RG)
