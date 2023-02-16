8ac518c06062af2cfc03e7eb2592dc479b261a17
snowcrab382 <108510951+snowcrab382@users.noreply.github.com>
2023-02-16 13:52:38 +0900

    [Silver I] Title: 스타트링크, Time: 508 ms, Memory: 70812 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -1,21 +1,20 @@
 from collections import deque
 
-F,S,G,U,D = map(int,input().split())
-visited = [0] * (F+1)
+f,s,g,u,d = map(int,input().split())
+floor = [0] * (f+1)
 
 def bfs(x):
     queue = deque()
     queue.append(x)
-    visited[x] = 1
+    floor[x] = 1
     while queue:
         x = queue.popleft()
-        if x == G:
-            return visited[G]-1
-        for X in (x+U,x-D):
-            if 1 <= X <= F and not visited[X]:
-                visited[X] = visited[x] + 1
-                queue.append(X)
+        if x == g:
+            return floor[g]-1
+        for i in (x+u,x-d):
+            if 1 <= i < f+1 and not floor[i]:
+                floor[i] = floor[x] + 1
+                queue.append(i)
+    return 'use the stairs'
 
-    return "use the stairs"
-
-print(bfs(S))
+print(bfs(s))
