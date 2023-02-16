edb109c2538e091fcdb8a21f556a230a113088cc
snowcrab382 <108510951+snowcrab382@users.noreply.github.com>
2023-02-16 12:10:40 +0900

    [Gold IV] Title: 불!, Time: 1352 ms, Memory: 39460 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.py
+++ 백준/Gold/4179. 불！/불！.py
@@ -1,45 +1,38 @@
-import sys
 from collections import deque
-input = sys.stdin.readline
+
+def bfs():
+    while queue:
+        time, x, y = queue.popleft()
+        if time > -1 and graph[x][y] != 'F' and (x == 0 or y == 0 or x == n - 1 or y == m - 1):
+            return time + 1
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] != '#':
+                if time > -1 and graph[nx][ny] == '.':
+                    graph[nx][ny] = '_'
+                    queue.append((time + 1, nx, ny))
+
+                elif time == -1 and graph[nx][ny] != 'F':
+                    graph[nx][ny] = 'F'
+                    queue.append((-1, nx, ny))
+    return 'IMPOSSIBLE'
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
 n, m = map(int, input().split())
-graph = []
+queue = deque()
 
+graph = []
 for i in range(n):
-    graph.append(list(input().rstrip()))
+    graph.append(list(input()))
     if 'J' in graph[i]:
-        q = deque([(0, i, graph[i].index('J'))])
+        queue.append((0,i,graph[i].index('J')))
 
 for i in range(n):
     for j in range(m):
         if graph[i][j] == 'F':
-            q.append((-1, i, j))
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-ans = 'IMPOSSIBLE'
-
-while q:
-    time, x, y = q.popleft()
-
-    # 지훈이 탈출
-    if time > -1 and graph[x][y] != 'F' and (x == 0 or y == 0 or x == n - 1 or y == m - 1):
-        ans = time + 1
-        break
-
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] != '#':
-
-            # 지훈이 이동
-            if time > -1 and graph[nx][ny] == '.':
-                graph[nx][ny] = '_'
-                q.append((time + 1, nx, ny))
-
-            # 불 퍼뜨리기
-            elif time == -1 and graph[nx][ny] != 'F':
-                graph[nx][ny] = 'F'
-                q.append((-1, nx, ny))
-
-print(ans)
+            queue.append((-1,i,j))
+print(bfs())
