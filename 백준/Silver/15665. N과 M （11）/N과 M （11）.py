968bca8984a2a68ce210e29f2034c5f51140bfae
snowcrab382 <108510951+snowcrab382@users.noreply.github.com>
2023-02-20 15:31:15 +0900

    [Silver II] Title: N과 M (11), Time: 1016 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.py
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -1,18 +1,19 @@
-n,m = map(int,input().split())
-graph = sorted(list(map(int,input().split())))
-ans = [0] * 10
-
-def solve(x):
+def bt(x):
     if x == m:
         for i in range(m):
-            print(ans[i], end=' ')
+            print(result[i], end=' ')
         print()
         return
 
     tmp = 0
-    for i in range(n):
-        if tmp != graph[i]:
-            ans[x] = graph[i]
-            tmp = ans[x]
-            solve(x+1)
-solve(0)
+    for i in range(len(graph)):
+        if graph[i] != tmp:
+            result[x] = graph[i]
+            tmp = result[x]
+            bt(x+1)
+
+n,m = map(int,input().split())
+graph = sorted(list(map(int,input().split())))
+result = [0] * (n+1)
+
+bt(0)
