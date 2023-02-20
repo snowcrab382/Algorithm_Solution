885c8c76dff0089e3b3999733b7cc661ad357a1a
snowcrab382 <108510951+snowcrab382@users.noreply.github.com>
2023-02-20 12:27:36 +0900

    [Silver III] Title: N과 M (8), Time: 60 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.py
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -1,21 +1,17 @@
-n,m = map(int,input().split())
-graph = sorted(list(map(int,input().split())))
-ans = [0] * 10
-visited = [0] * 10
+n, m = map(int, input().split())
+my_list = list(map(int, input().split()))
+my_list.sort()
+solve = []
 
-def solve(x):
-    if x == m:
-        for i in range(m):
-            print(graph[ans[i]], end=' ')
-        print()
+def Dfs(depth):
+    if depth == m:
+        print(' '.join(map(str, solve)))
         return
-    st = 0
-    if x != 0:
-        st = ans[x-1]
-    for i in range(st,n):
-        ans[x] = i
-        visited[i] = 1
-        solve(x+1)
-        visited[i] = 0
 
-solve(0)
+    for i in range(n):
+        if depth == 0 or solve[depth - 1] <= my_list[i]:
+            solve.append(my_list[i])
+            Dfs(depth + 1)
+            solve.pop()
+
+Dfs(0)
