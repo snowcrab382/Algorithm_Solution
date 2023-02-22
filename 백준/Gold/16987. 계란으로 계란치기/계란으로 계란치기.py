11465d8bfa672ad9df863e6dc44075b6f9a52d4a
snowcrab382 <108510951+snowcrab382@users.noreply.github.com>
2023-02-22 13:31:17 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 4824 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+def bt(x):
+    global max_cnt
+    cnt = 0
+    if x == n:
+        for i in graph:
+            if i[0] <= 0:
+                cnt += 1
+        max_cnt = max(max_cnt,cnt)
+        return
+
+    if graph[x][0] <= 0:
+        bt(x+1)
+    else:
+        flag = False
+        for i in range(n):
+            if graph[i][0] <= 0 or x == i:
+                continue
+            graph[x][0] -= graph[i][1]
+            graph[i][0] -= graph[x][1]
+            flag = True
+            bt(x+1)
+            graph[x][0] += graph[i][1]
+            graph[i][0] += graph[x][1]
+        if not flag:
+            bt(x+1)
+
+n = int(input())
+graph = [list(map(int,input().split())) for _ in range(n)]
+max_cnt = 0
+bt(0)
+print(max_cnt)
