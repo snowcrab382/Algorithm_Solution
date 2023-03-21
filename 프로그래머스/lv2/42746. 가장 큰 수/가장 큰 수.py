fb787a6bf93e7fe44809c69ebfce957900d8bb3f
snowcrab382 <108510951+snowcrab382@users.noreply.github.com>
2023-03-21 12:19:42 +0900

    [level 2] Title: 가장 큰 수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,9 +1,13 @@
-# def solution(numbers):
-#     numbers = [str(i) for i in numbers]
-#     numbers.sort(key = lambda x : x*3,reverse = True)
+def solution(numbers):
+    numbers = [str(i) for i in numbers]
+    numbers.sort(key = lambda x : x*3,reverse = True)
     
-#     return ''.join(numbers)
-def solution(numbers):    
-    s = list(map(str,numbers))
-    a = sorted(s,key=lambda x: x*3,reverse=1)
-    return str(int("".join(a)))
+    result = ''.join(numbers)
+    if result[0] == '0':
+        return '0'
+    else:
+        return result
+# def solution(numbers):    
+#     s = list(map(str,numbers))
+#     a = sorted(s,key=lambda x: x*3,reverse=1)
+#     return str(int("".join(a)))
