a6c98a096296b9267de6534b57c2928d0295661c
snowcrab382 <108510951+snowcrab382@users.noreply.github.com>
2023-03-28 11:42:08 +0900

    [unrated] Title: 숫자 카드 나누기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.py
@@ -0,0 +1,45 @@
+def solution(arrayA, arrayB):
+    div_A,div_B = [],[]
+    answer = []
+    for i in range(1,int((arrayA[0])**0.5)+1):
+        if arrayA[0] % i == 0:
+            div_A.append(i)
+            div_A.append(arrayA[0]//i)
+    for j in range(1,int((arrayB[0])**0.5)+1):
+        if arrayB[0] % j == 0:
+            div_B.append(j)
+            div_B.append(arrayB[0]//j)
+    
+    for x in div_A:
+        tmp = True
+        for y in arrayA:
+            if y % x != 0:
+                tmp = False
+                break
+        if tmp:
+            for z in arrayB:
+                if z % x == 0:
+                    tmp = False
+                    break
+            if tmp:
+                answer.append(x)
+
+    for x in div_B:
+        tmp = True
+        for y in arrayB:
+            if y % x != 0:
+                tmp = False
+                break
+        if tmp:
+            for z in arrayA:
+                if z % x == 0:
+                    tmp = False
+                    break
+            if tmp:
+                answer.append(x)
+        
+    if len(answer):
+        return max(answer)
+    else:
+        return 0
+            
