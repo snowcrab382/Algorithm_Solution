86f8f4f9019195846571ef10e9f918056f8dc510
임동훈 <108510951+snowcrab382@users.noreply.github.com>
2023-10-10 16:04:21 +0900

    [Bronze II] Title: 시험 감독, Time: 572 ms, Memory: 146464 KB -BaekjoonHub

--- 백준/Bronze/13458. 시험 감독/시험 감독.py
+++ 백준/Bronze/13458. 시험 감독/시험 감독.py
@@ -1,17 +1,14 @@
-import sys
-input = sys.stdin.readline
-N = int(input())
-A = map(int,input().split())
-B, C = map(int,input().split())
-count = 0
-for i in A:
-    if i <= B:
-        count += 1
-    else:
-        i -= B
-        count += 1
-        if i % C == 0:
-            count += i // C
-        else:
-            count += (i // C) + 1
-print(count)
+import math
+
+n = int(input())
+a = list(map(int,input().split()))
+b,c = map(int,input().split())
+result = 0
+
+for i in a:
+  if i <= b:
+    result += 1
+    continue
+  else:
+    result += math.ceil((i-b) / c) + 1
+print(result)
