7a0c73d7c3d51e90773c4dd657ebe5179ada7045
임동훈 <108510951+snowcrab382@users.noreply.github.com>
2023-10-10 17:04:47 +0900

    [Silver I] Title: 그림, Time: 324 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,44 +1,34 @@
 from collections import deque
 
-n, m = map(int, input().split())
-graph = []
+n,m = map(int,input().split())
+graph = [list(input().split()) for _ in range(n)]
+result = {0}
+count = 0
 
-for i in range(n):
-    graph.append(list(map(int, input().split())))
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
 
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
-
-
-def bfs(graph, a, b):
+def bfs(x,y):
     queue = deque()
-    queue.append((a, b))
-    graph[a][b] = 0
-    count = 1
-
+    queue.append((x,y))
+    area = 1
     while queue:
-        x, y = queue.popleft()
+        x,y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = 0
-                queue.append((nx, ny))
-                count += 1
-    return count
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == '1':
+                queue.append((nx,ny))
+                graph[nx][ny] = '0'
+                area += 1
+    result.add(area)
 
-
-paint = []
 for i in range(n):
     for j in range(m):
-        if graph[i][j] == 1:
-            paint.append(bfs(graph, i, j))
+        if graph[i][j] == '1':
+            graph[i][j] = '0'
+            bfs(i,j)
+            count += 1
 
-if len(paint) == 0:
-    print(len(paint))
-    print(0)
-else:
-    print(len(paint))
-    print(max(paint))
+print(count)
+print(max(result))
