3f2195a646a234d9b61570dbf60291d83d3175c8
임동훈 <108510951+snowcrab382@users.noreply.github.com>
2023-10-11 11:56:24 +0900

    [Gold V] Title: 토마토, Time: 1280 ms, Memory: 98576 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -2,37 +2,35 @@ from collections import deque
 import sys
 
 input = sys.stdin.readline
-n,m = map(int,input().split())
-graph = []
-queue = deque()
 
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
 
-for i in range(m):
-    line = list(map(int,input().split()))
-    graph.append(line)
-    for j in range(n):
-        if graph[i][j] == 1:
-            queue.append((i,j))
-
-def bfs():
+def bfs(queue):
     while queue:
         x,y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < m and 0 <= ny < n and graph[nx][ny] == 0:
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
                 graph[nx][ny] = graph[x][y] + 1
                 queue.append((nx,ny))
 
-bfs()
-result = 0
+m,n = map(int,input().split())
+graph = [list(map(int,input().split())) for _ in range(n)]
+queue = deque()
 
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 1:
+            queue.append((i,j))
+
+bfs(queue)
+result = 0
 for i in graph:
     for j in i:
-        if j== 0:
+        if j == 0:
             print(-1)
             exit(0)
-    result = max(result,max(i))
+    result = max(result, max(i))
 print(result-1)
