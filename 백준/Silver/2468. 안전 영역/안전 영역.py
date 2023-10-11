463cadaf44925db1ba43fc246bb680ae2b7262de
임동훈 <108510951+snowcrab382@users.noreply.github.com>
2023-10-11 15:55:23 +0900

    [Silver I] Title: 안전 영역, Time: 884 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,43 +1,47 @@
 from collections import deque
-import copy
 
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
-
-N = int(input())
+n = int(input())
+max_num = 0
 graph = []
-for _ in range(N):
-    graph.append(list(map(int,input().split())))
 
-def bfs(x,y):
+for _ in range(n):
+    low = list(map(int, input().split()))
+    graph.append(low)
+
+    for i in low:
+        if i > max_num:
+            max_num = i
+
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
+
+def bfs(x, y, num):
     queue = deque()
-    queue.append((x,y))
-    graph_copy[x][y] = 0
+    queue.append((x, y))
+    visited[x][y] = 1
+
     while queue:
-        x,y = queue.popleft()
+        x, y = queue.popleft()
+
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < N and 0 <= ny < N and graph_copy[nx][ny] != 0:
-                graph_copy[nx][ny] = 0
-                queue.append((nx,ny))
+            nx = dx[i] + x
+            ny = dy[i] + y
+
+            if 0 <= nx < n and 0 <= ny < n:
+                if graph[nx][ny] > num and visited[nx][ny] == 0:
+                    visited[nx][ny] = 1
+                    queue.append((nx, ny))
 
 result = []
-for a in range(1,101):
-    graph_copy = copy.deepcopy(graph)
-    for i in range(N):
-        for j in range(N):
-            if graph_copy[i][j] <= a:
-                graph_copy[i][j] = 0
 
-    count = 0
-    for i in range(N):
-        for j in range(N):
-            if graph_copy[i][j] != 0:
-                bfs(i,j)
-                count += 1
-    result.append(count)
-if max(result) == 0:
-    print(1)
-else:
-    print(max(result))
+for i in range(max_num):
+    cnt = 0
+    visited = [[0]*n for _ in range(n)]
+    for j in range(n):
+        for k in range(n):
+            if graph[j][k] > i and visited[j][k] == 0:
+                bfs(j, k, i)
+                cnt += 1
+    result.append(cnt)
+
+print(max(result))
