428ebd69c12d9dd241774afccd44e291a06d3b82
임동훈 <108510951+snowcrab382@users.noreply.github.com>
2023-10-11 13:25:46 +0900

    [Silver I] Title: 단지번호붙이기, Time: 68 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,10 +1,8 @@
 from collections import deque
 
-N = int(input())
-graph = []
-
-for _ in range(N):
-    graph.append(list(map(int,input())))
+n = int(input())
+graph = [list(map(int,input())) for _ in range(n)]
+result = []
 
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
@@ -19,19 +17,16 @@ def bfs(x,y):
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < N and 0 <= ny < N and graph[nx][ny] == 1:
+            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 1:
                 graph[nx][ny] = 0
-                count += 1
                 queue.append((nx,ny))
+                count += 1
     return count
 
-paint = []
-for i in range(N):
-    for j in range(N):
+for i in range(n):
+    for j in range(n):
         if graph[i][j] == 1:
-            paint.append(bfs(i,j))
-
-paint.sort()
-print(len(paint))
-for i in paint:
-    print(i)
+            result.append(bfs(i,j))
+print(len(result))
+for k in sorted(result):
+    print(k)
