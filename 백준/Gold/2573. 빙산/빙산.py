27cb9849108fd09019f0fd876e6474b58a8e0165
임동훈 <108510951+snowcrab382@users.noreply.github.com>
2023-10-11 18:33:04 +0900

    [Gold IV] Title: 빙산, Time: 716 ms, Memory: 213728 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,65 +1,54 @@
 from collections import deque
-import sys
-
-input = sys.stdin.readline
-
-def melting():
-    while queue:
-        cnt = 0
-        x,y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
-                cnt += 1
-        melt.append((x,y,cnt))
-
-def bfs(x,y):
-    check = deque()
-    check.append((x,y))
-    while check:
-        x,y = check.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0<=nx<n and 0<=ny<m and not visited[nx][ny] and graph[nx][ny] != 0:
-                visited[nx][ny] = True
-                check.append((nx,ny))
 
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
 
 n,m = map(int,input().split())
 graph = [list(map(int,input().split())) for _ in range(n)]
+day = 0
+check = False
 queue = deque()
-year = 0
 
-for i in range(1,n-1):
-    for j in range(1,m-1):
-        if graph[i][j] != 0:
-            queue.append((i,j))
+def bfs(x,y):
+    queue.append((x,y))
+    while queue:
+        x,y = queue.popleft()
+        visited[x][y] = 1
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m:
+                if graph[nx][ny] != 0 and not visited[nx][ny]:
+                    queue.append((nx,ny))
+                    visited[nx][ny] = 1
+                elif graph[nx][ny] == 0:
+                    count[x][y] += 1
 
-while queue:
-    melt = []
-    visited = [[False] * m for _ in range(n)]
+
+while True:
+    count = [[0] * m for _ in range(n)]
+    visited = [[0] * m for _ in range(n)]
     group = 0
-    year += 1
-
-    melting()
-    for i,j,k in melt:
-        if graph[i][j] - k <= 0:
-            graph[i][j] = 0
-        else:
-            graph[i][j] -= k
-            queue.append((i,j))
-
-    for i in range(1,n-1):
-        for j in range(1,m-1):
+    for i in range(n):
+        for j in range(m):
             if graph[i][j] != 0 and not visited[i][j]:
                 bfs(i,j)
-                visited[i][j] = True
                 group += 1
-    if group > 1:
-        print(year)
-        exit(0)
-print(0)
+
+    for i in range(n):
+        for j in range(m):
+            graph[i][j] -= count[i][j]
+            if graph[i][j] < 0:
+                graph[i][j] = 0
+
+    if group == 0:
+        break
+    if group >= 2:
+        check = True
+        break
+    day += 1
+
+if check:
+    print(day)
+else:
+    print(0)
