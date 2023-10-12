86c567c05b343351fd2b5a3e8707b790bc105408
임동훈 <108510951+snowcrab382@users.noreply.github.com>
2023-10-12 14:49:21 +0900

    [Silver III] Title: 퇴사, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,17 +1,9 @@
-def bt(day, tmp):
-    global ans
-    if day == N:
-        if ans < tmp:
-            ans = tmp
-        return
-    bt(day+1, tmp)
-    if day + a[day][0] <= N:
-        bt(day+a[day][0], tmp + a[day][1])
+n = int(input())
+schedule = [list(map(int,input().split())) for _ in range(n)]
+dp = [0] * (n+1)
 
-N = int(input())
-a = []
-for _ in range(N):
-    a.append(list(map(int,input().split())))
-ans = 0
-bt(0, 0)
-print(ans)
+for i in range(n):
+    for j in range(i+schedule[i][0], n+1):
+        if dp[j] < dp[i] + schedule[i][1]:
+            dp[j] = dp[i] + schedule[i][1]
+print(dp[-1])
