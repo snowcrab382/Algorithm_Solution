56cee5accd0107dc5ae5e99b44b1c35ad785d682
임동훈 <108510951+snowcrab382@users.noreply.github.com>
2023-10-13 21:10:11 +0900

    [Gold I] Title: 낚시왕, Time: 2864 ms, Memory: 226280 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.py
+++ 백준/Gold/17143. 낚시왕/낚시왕.py
@@ -4,27 +4,26 @@ for _ in range(M):
     r,c,s,d,z = map(int,input().split())
     graph[r-1][c-1].append([s,d,z])
 
-#위,아래,오른쪽,왼쪽 순서
 dx = [0,-1,1,0,0]
 dy = [0,0,0,1,-1]
 
-def catch(y,graph):
+def catch(y):
     size = 0
     for i in range(R):
         if graph[i][y]:
-            size = graph[i][y][0][2]
+            size += graph[i][y][0][2]
             graph[i][y] = []
             break
-    return size,graph
+    return size
 
 def move(graph):
     temp = [[[] for _ in range(C)] for _ in range(R)]
-    for x in range(R):
-        for y in range(C):
-            if graph[x][y]:
-                s,d,z = graph[x][y][0]
+    for i in range(R):
+        for j in range(C):
+            if graph[i][j]:
+                s,d,z = graph[i][j][0]
+                nx,ny = i,j
                 dist = s
-                nx,ny = x,y
                 while dist:
                     nx += dx[d]
                     ny += dy[d]
@@ -42,14 +41,12 @@ def move(graph):
     for i in range(R):
         for j in range(C):
             if len(temp[i][j]) > 1:
-                temp[i][j].sort(key = lambda x : x[2], reverse = True)
-                s,d,z = temp[i][j][0]
-                temp[i][j] = [[s,d,z]]
+                temp[i][j].sort(key = lambda x : -x[2])
     return temp
+
 
 result = 0
 for i in range(C):
-    size,graph = catch(i,graph)
-    result += size
+    result += catch(i)
     graph = move(graph)
 print(result)
