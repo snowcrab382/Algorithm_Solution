8b9f81e7b7e891abef6b2e071039cd140a0b3b90
임동훈 <snowcrab382@naver.com>
2024-04-08 13:51:11 +0900

    [Gold IV] Title: 치즈, Time: 88 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.py
+++ 백준/Gold/2636. 치즈/치즈.py
@@ -1,38 +1,39 @@
 from collections import deque
 
 n, m = map(int,input().split())
-cheese = [list(map(int,input().split())) for _ in range(n)]
+graph = [list(map(int,input().split())) for _ in range(n)]
 
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
 
-def dfs(x,y):
-    visited = [[0] * m for _ in range(n)]
-    cnt = 0
-    q = deque([(x,y)])
-    visited[x][y] = 1
+def bfs(i,j):
+    q = deque([(i,j)])
+    visited[i][j] = 1
+    tmp = []
     while q:
         x,y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+        for dir in range(4):
+            nx = x + dx[dir]
+            ny = y + dy[dir]
             if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
-                visited[nx][ny] = 1
-                if cheese[nx][ny]:
-                    cheese[nx][ny] = 0
-                    cnt += 1
+                if graph[nx][ny] == 1:
+                    tmp.append((nx,ny))
                 else:
                     q.append((nx,ny))
-    return cnt
+                visited[nx][ny] = 1
 
-time = 0
+    for r,c in tmp:
+        graph[r][c] = 0
+    return len(tmp)
+
 cnt = 0
+cheese = 0
 while True:
-    tmp = dfs(0,0)
+    visited = [[0] * m for _ in range(n)]
+    tmp = bfs(0,0)
     if tmp == 0:
-        print(time)
         print(cnt)
+        print(cheese)
         break
-    else:
-        time += 1
-        cnt = tmp
+    cheese = tmp
+    cnt += 1
