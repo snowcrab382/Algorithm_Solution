2ee0a396ab03100d67c9946562cbaf394d351483
임동훈 <snowcrab382@naver.com>
2024-04-08 13:13:24 +0900

    [Gold IV] Title: 인구 이동, Time: 6240 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -1,6 +1,6 @@
 from collections import deque
 
-n, l ,r = map(int,input().split())
+n, l, r = map(int,input().split())
 ground = [list(map(int,input().split())) for _ in range(n)]
 
 dx = [1,-1,0,0]
@@ -12,36 +12,38 @@ def check(ground):
     for i in range(n):
         for j in range(n):
             if not visited[i][j]:
-                q = deque([(i,j)])
-                tmp = [(i,j)]
+                union = [(i, j)]
+                q = deque([(i, j)])
                 visited[i][j] = 1
                 while q:
-                    x,y = q.popleft()
-                    for k in range(4):
-                        nx = x + dx[k]
-                        ny = y + dy[k]
+                    x, y = q.popleft()
+                    for dir in range(4):
+                        nx = x + dx[dir]
+                        ny = y + dy[dir]
                         if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
                             if l <= abs(ground[x][y] - ground[nx][ny]) <= r:
                                 visited[nx][ny] = 1
-                                tmp.append((nx,ny))
-                                q.append((nx,ny))
-                unions.append(tmp)
+                                q.append((nx, ny))
+                                union.append((nx, ny))
+                unions.append(union)
     return unions
 
 def move(unions):
     for union in unions:
-        sum = 0
+        total = 0
         for x,y in union:
-            sum += ground[x][y]
-        people = sum // len(union)
+            total += ground[x][y]
+        people = total // len(union)
         for x,y in union:
             ground[x][y] = people
 
 cnt = 0
 while True:
     unions = check(ground)
+    
     if len(unions) == n**2:
         print(cnt)
         break
+
     move(unions)
     cnt += 1
