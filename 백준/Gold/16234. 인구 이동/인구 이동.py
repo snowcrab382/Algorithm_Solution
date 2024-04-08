446fb312877da587a51075f2c9b7f4da811be6d5
임동훈 <snowcrab382@naver.com>
2024-04-08 13:15:57 +0900

    [Gold IV] Title: 인구 이동, Time: 1404 ms, Memory: 203020 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -1,34 +1,38 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
 n, l, r = map(int,input().split())
 ground = [list(map(int,input().split())) for _ in range(n)]
 
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
-
-def check(ground):
+    
+cnt = 0
+while True:
     visited = [[0] * n for _ in range(n)]
     unions = []
     for i in range(n):
         for j in range(n):
             if not visited[i][j]:
-                union = [(i, j)]
-                q = deque([(i, j)])
+                union = [(i,j)]
+                q = deque([(i,j)])
                 visited[i][j] = 1
                 while q:
-                    x, y = q.popleft()
+                    x,y = q.popleft()
                     for dir in range(4):
                         nx = x + dx[dir]
                         ny = y + dy[dir]
                         if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
                             if l <= abs(ground[x][y] - ground[nx][ny]) <= r:
                                 visited[nx][ny] = 1
-                                q.append((nx, ny))
-                                union.append((nx, ny))
+                                q.append((nx,ny))
+                                union.append((nx,ny))
                 unions.append(union)
-    return unions
+    if len(unions) == n**2:
+        print(cnt)
+        break
 
-def move(unions):
     for union in unions:
         total = 0
         for x,y in union:
@@ -36,14 +40,4 @@ def move(unions):
         people = total // len(union)
         for x,y in union:
             ground[x][y] = people
-
-cnt = 0
-while True:
-    unions = check(ground)
-    
-    if len(unions) == n**2:
-        print(cnt)
-        break
-
-    move(unions)
     cnt += 1
