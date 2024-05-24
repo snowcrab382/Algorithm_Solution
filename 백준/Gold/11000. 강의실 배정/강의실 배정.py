bc5c357e628f8dc1011c0c999fb973866ed63be1
임동훈 <snowcrab382@naver.com>
2024-05-24 13:37:41 +0900

    [Gold V] Title: 강의실 배정, Time: 464 ms, Memory: 86516 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -0,0 +1,16 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+schedule = []
+for _ in range(n):
+    schedule.append(list(map(int,input().split())))
+schedule.sort(key = lambda x : (x[0], x[1]))
+
+heap = [schedule[0][1]]
+for i in range(1,n):
+    if heap[0] <= schedule[i][0]:
+        heapq.heappop(heap)
+    heapq.heappush(heap, schedule[i][1])
+print(len(heap))
