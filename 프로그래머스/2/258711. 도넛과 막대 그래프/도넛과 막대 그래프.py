ef71f34daf597c8537ac997555f5b5d45b692e77
임동훈 <snowcrab382@naver.com>
2024-07-05 10:45:44 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 73.07 ms, Memory: 25.4 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
@@ -1,40 +1,24 @@
-import sys
-sys.setrecursionlimit(10**6)
-
 def solution(edges):
-    def check(start, tmp):
-        if not graph[tmp]:
-            answer[2] += 1
-            return
-        
-        if len(graph[tmp]) == 2:
-            answer[3] += 1
-            return
-        
-        for node in graph[tmp]:
-            if start == node:
-                answer [1] += 1
-                return
-            
-            check(start, node)
-            
-        
-    answer = [0,0,0,0]
-    graph = [[] for _ in range(1000001)]
-    visited = [0] * 1000001
-    
-    for a,b in edges:
-        graph[a].append(b)
-        visited[b] = 1
-    
-    max_cnt = 0
-    for i in range(len(graph)):
-        if len(graph[i]) > max_cnt and not visited[i]:
-            answer[0] = i
-            max_cnt = len(graph[i])
+    input_count = [0 for _ in range(1000001)]
+    output_count = [0 for _ in range(1000001)]
+    created_node = -1
+    doughnut_count = 0
+    stick_count = 0
+    eight_count = 0
+    biggest_node_num = -1
 
-    for i in graph[answer[0]]:
-        check(i, i)
-            
-    return answer
+    for a, b in edges:
+        biggest_node_num = max(biggest_node_num, a, b)
+        output_count[a] += 1
+        input_count[b] += 1
 
+    for i in range(1, biggest_node_num + 1):
+        if input_count[i] == 0 and output_count[i] >= 2:
+            created_node = i
+        elif input_count[i] >= 1 and output_count[i] == 0:
+            stick_count += 1
+        elif input_count[i] >= 2 and output_count[i] == 2:
+            eight_count += 1
+
+    doughnut_count = output_count[created_node] - stick_count - eight_count
+    return [created_node, doughnut_count, stick_count, eight_count]
