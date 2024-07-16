c82fcda7f3e4b44b47815e201a722cf04cc8c6c3
임동훈 <snowcrab382@naver.com>
2024-07-16 10:35:22 +0900

    [level 3] Title: 입국심사, Time: 896.70 ms, Memory: 14.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -0,0 +1,19 @@
+import sys
+MAX = sys.maxsize
+
+def solution(n, times):
+    s, e = 0, MAX
+    while s < e:
+        m = (s + e) // 2
+        
+        tmp = 0
+        for time in times:
+            tmp += m // time
+        
+        if tmp >= n:
+            e = m
+        else:
+            s = m + 1
+        
+    
+    return s
