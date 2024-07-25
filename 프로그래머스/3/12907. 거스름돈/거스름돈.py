90475108c20830d18e31c17333c9df0d4bed1fc2
임동훈 <snowcrab382@naver.com>
2024-07-25 10:28:24 +0900

    [level 3] Title: 거스름돈, Time: 443.55 ms, Memory: 13.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.py
@@ -0,0 +1,9 @@
+def solution(n, money):
+    dp = [0] * (n+1)
+    dp[0] = 1
+    
+    for m in money :
+        for price in range(m, n+1) :
+            dp[price] += dp[price - m]
+
+    return dp[-1] % 1000000007
