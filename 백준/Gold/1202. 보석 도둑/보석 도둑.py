13e36f822b29d4ad6b89991c806e993e9843dfe0
임동훈 <snowcrab382@naver.com>
2024-10-08 12:22:37 +0900

    [Gold II] Title: 보석 도둑, Time: 1412 ms, Memory: 86552 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -2,18 +2,19 @@ import heapq
 import sys
 input = sys.stdin.readline
 
-n, k = map(int,input().split())
-jewels = [list(map(int,input().split())) for _ in range(n)]
+n, k = map(int, input().split())
+j = [tuple(map(int, input().split())) for _ in range(n)]
 bags = [int(input()) for _ in range(k)]
-jewels.sort()
+j.sort()
 bags.sort()
 
 result = 0
 tmp = []
 for bag in bags:
-    while jewels and jewels[0][0] <= bag:
-        heapq.heappush(tmp, -jewels[0][1])
-        heapq.heappop(jewels)
+    while j and j[0][0] <= bag:
+        heapq.heappush(tmp, -j[0][1])
+        heapq.heappop(j)
+
     if tmp:
         result -= heapq.heappop(tmp)
 print(result)
